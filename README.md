30a5e86821c36a9efd6b867f000e82a6f9e49865
kamilrogala <contact@kamilrogala.it>
2018-04-07 19:58:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project: ajaxTesting
+
+Just small test with ajax and test for ajax preloader
+
+Keep scrolling...
+
+...or [check it](https://kamilrogala.github.io/ajaxTesting/)!
---
+++ docs/js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$('.loader-bg').hide();
+	$('a').click(function (e) {
+		var itemHref = $(this).attr('href');
+		if (itemHref !== 'index.html') {
+			e.preventDefault();
+			$('.loader-bg').fadeIn(300);
+			$('body').css({
+				'overflow': 'hidden'
+			})
+			setTimeout(function () {
+				$.ajax({
+						url: itemHref,
+						method: 'GET',
+						dataType: 'html'
+					})
+					.done(function (res) {
+						$('#result').html(res);
+						setTimeout(function () {
+							$('.loader-bg').fadeOut(300);
+							$('body').css({
+								'overflow': 'auto'
+							})
+						}, 500);
+					});
+			}, 1300);
+		}
+	});
+});
