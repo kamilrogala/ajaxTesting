7499c86cc04c5d84f177a5480052937fb00bce60
kamilrogala <contact@kamilrogala.it>
2018-04-07 20:04:53 +0200

    Preloader animation added on start

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
-	$('.loader-bg').hide();
-	$('a').click(function (e) {
-		var itemHref = $(this).attr('href');
-		if (itemHref !== 'index.html') {
-			e.preventDefault();
-			$('.loader-bg').fadeIn(300);
-			$('body').css({
-				'overflow': 'hidden'
-			})
-			setTimeout(function () {
-				$.ajax({
-						url: itemHref,
-						method: 'GET',
-						dataType: 'html'
-					})
-					.done(function (res) {
-						$('#result').html(res);
-						setTimeout(function () {
-							$('.loader-bg').fadeOut(300);
-							$('body').css({
-								'overflow': 'auto'
-							})
-						}, 500);
-					});
-			}, 1300);
-		}
-	});
+	setTimeout(function () {
+		$('.loader-bg').fadeOut(300);
+		$('a').click(function (e) {
+			var itemHref = $(this).attr('href');
+			if (itemHref !== 'index.html') {
+				e.preventDefault();
+				$('.loader-bg').fadeIn(300);
+				$('body').css({
+					'overflow': 'hidden'
+				})
+				setTimeout(function () {
+					$.ajax({
+							url: itemHref,
+							method: 'GET',
+							dataType: 'html'
+						})
+						.done(function (res) {
+							$('#result').html(res);
+							setTimeout(function () {
+								$('.loader-bg').fadeOut(300);
+								$('body').css({
+									'overflow': 'auto'
+								})
+							}, 500);
+						});
+				}, 1300);
+			}
+		});
+	},1000);
 });
